170f533d3c87a7e1bbbc210d09a5746943a12fe1
lndsilva <lndsilva@hotmail.com>
2024-11-11 20:14:41 -0300

    projeto controle de estoque php p2

--- index.php
+++ index.php
@@ -4,12 +4,34 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Home</title>
+	 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	 <link rel="stylesheet" type="text/css" href="css/estilo.css">
+
 </head>
 <body>
 
-	<ul>
-		<li><a href=""></a></li>
-	</ul>
+	<header>
+		<img src="imagens/logo.png" alt="logo">
+		<nav>
+			<ul>
+				<li><a href="#">Home</li>
+				<li><a href="#">Produtos</li>
+				<li><a href="#">Fornecedores</li>
+				<li><a href="#">Categorias</li>
+			</ul>
+		</nav>
+	</header>
+	<main class="container">
+		<h2>Conteúdo</h2>
+	</main>
+	<footer>
+		<h2>Rodapé</h2>
+	</footer>
 
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </body>
 </html>
