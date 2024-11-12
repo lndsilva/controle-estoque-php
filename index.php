ebb16c55b014ff2b760557ce23bd72a1729eade7
lndsilva <lndsilva@hotmail.com>
2024-11-11 21:47:58 -0300

    projeto controle de estoque php bootstrap

--- index.php
+++ index.php
@@ -1,31 +1,42 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Home</title>
-	 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-	 <link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
 
 </head>
+
 <body>
 
 	<header>
-		<img src="imagens/logo.png" alt="logo">
-		<nav class="navbar navbar-expand-lg bg-body-tertiary">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container-fluid">
-				<ul>
-					<li><a href="#">Home</a></li>
-					<li><a href="#">Produtos</a></li>
-					<li><a href="#">Fornecedores</a></li>
-					<li><a href="#">Categorias</a></li>
-				</ul>
+				<a class="navbar-brand" href="index.php">
+					<img src="imagens/logo.png" alt="Logotipo" width="90" height="50">
+				</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+					aria-controls="navbarNav" aria-expanded="false" aria-label="Alternar navegação">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul class="navbar-nav ms-auto">
+						<li class="nav-item"><a href="#" class="nav-link active" aria-current="home">Home</a></li>
+						<li class="nav-item"><a href="#" class="nav-link">Produtos</a></li>
+						<li class="nav-item"><a href="#" class="nav-link">Fornecedores</a></li>
+						<li class="nav-item"><a href="#" class="nav-link">Categorias</a></li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	</header>
 	<main class="container">
 		<h2>Conteúdo</h2>
-		
+
 
 	</main>
 	<footer>
@@ -35,7 +46,14 @@
 
 
 
-	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
+
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+		integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+		crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+		integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+		crossorigin="anonymous"></script>
 </body>
+
 </html>
